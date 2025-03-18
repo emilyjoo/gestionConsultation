dad34da5809e4ee4a668abca922113938fe8b989
Fatima Zahra Sabri <fzsabri3@gmail.com>
2025-03-18 21:36:33 +0100

    update

---
+++ src/main/java/ma/enset/gestionconsultation/web/HomeController.java
@@ -0,0 +1,18 @@
+package ma.enset.gestionconsultation.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String accueil() {
+        return "accueil";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+}
